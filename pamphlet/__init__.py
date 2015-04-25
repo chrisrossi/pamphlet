ef98339066edeb1c4162fee9730ddc8d7a5f927f
Chris Rossi <chris@archimedeanco.com>
2015-04-25 15:02:03 -0400

    Basic tween.

--- pamphlet/__init__.py
+++ pamphlet/__init__.py
@@ -1,7 +1,9 @@
 import acidfs
+import base64
 import dumpling
+import re
 
-from pyramid.view import view_config
+from pyramid.renderers import render_to_response
 
 
 @dumpling.folder
@@ -23,6 +25,29 @@ def root_finder(root_folder_factory=HomePage):
     return factory
 
 
+BODY = re.compile('<body([^>]*)>')
+
+def pamphlet_tween_factory(handler, registry):
+    def pamphlet_tween(request):
+        response = handler(request)
+        if response.status_int == 200 and response.content_type == 'text/html':
+            onload = (' onload="parent.postMessage('
+                      'document.body.scrollHeight, \'%s\');"' %
+                      request.url)
+            text = BODY.sub(
+                lambda m: '<body%s%s>'% (m.groups()[0], onload),
+                response.text).encode('utf8')
+            return render_to_response(
+                'templates/contentbar.pt',
+                {'contentsrc': 'data:text/html;charset=utf8;base64,' +
+                               str(base64.b64encode(text), 'utf8')},
+                request=request,
+                response=response)
+        return response
+    return pamphlet_tween
+
+
 def includeme(config):
     config.set_root_factory(root_finder())
+    config.add_tween('pamphlet.pamphlet_tween_factory')
     config.scan('.')
