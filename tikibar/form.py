44b2c183b7c75e37b73afce8402009bc812bccfc
Chris Rossi <chris@archimedeanco.com>
2015-05-01 22:39:50 -0400

    Add parent nodes, self closing tags.

--- tikibar/form.py
+++ tikibar/form.py
@@ -1,20 +1,29 @@
 
 class tag(object):
+    self_closing = False
 
     def __init__(self, name, **attrs):
+        if name.endswith('/'):
+            self.self_closing = True
+            name = name[:-1]
         self.name = name
         self.attrs = attrs
         self.children = ()
 
     def __call__(self, *children):
-        def totext(x):
+        def mkchild(x):
             if isinstance(x, str):
                 x = text(x)
+            x.parent = self
             return x
-        self.children = tuple(totext(x) for x in children)
+        self.children = tuple(mkchild(x) for x in children)
         return self
 
     def _render(self):
+        if self.self_closing and not self.children:
+            yield '<%s/>' % self.name
+            return
+
         if self.attrs:
             attrs = ' ' + ' '.join(
                 ('%s="%s"' % (k.lstrip('_'), v) for k, v in self.attrs.items())
@@ -32,14 +41,18 @@ class tag(object):
     __html__ = __str__  # chameleon
 
     def __repr__(self):
+        if self.self_closing and not self.children:
+            return 'tag(%s)' % repr(self.name + '/')
+
         if self.attrs:
             attrs = ', ' + ', '.join(
                 ('%s=%s' % (k, repr(v)) for k, v in self.attrs.items())
             )
         else:
             attrs = ''
-        children = ', '.join(map(repr, self.children))
-        return 'tag(%s%s)(%s)' % (repr(self.name), attrs, children)
+        children = ('(%s)' % ', '.join(map(repr, self.children))
+                    if self.children else '')
+        return 'tag(%s%s)%s' % (repr(self.name), attrs, children)
 
 
 class text(str):
